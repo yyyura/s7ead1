bb3299dd1568fa1aa301c4bda46ca3f0bc1bd23b
yyyura <yyyura@gmail.com>
2017-09-17 22:13:18 +0100

    start

---
+++ s7ead1/s7ead1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s7ead1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
